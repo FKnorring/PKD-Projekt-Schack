90958c9cec196ffaa82e5784b3d5dc98777f34c5
FKnorring <71324888+FKnorring@users.noreply.github.com>
2021-02-15 17:13:34 +0100

    Update projekt.hs

--- projekt.hs
+++ projekt.hs
@@ -2,8 +2,10 @@ import Data.Char
 
 type Board = [[Square]]
 
-data Square = Empty | Piece
+{-data Square represents if a certain square is empty or contains a piece-}
+data Square = Empty | Occupied Piece
 
+{- data Piece represents the specific piece-}
 data Piece = Piece PColor PType
 
 {-data PType represents the different piece types-}
@@ -12,25 +14,25 @@ data PType = Bishop | Pawn | Rook | Knight | King | Queen
 {- data PColor represent the pieces color-}
 data PColor = White | Black
 
-{-data Square represents if a certain square is empty or contains a piece-}
 
 
-{- data Piece represents the specific piece-}
 
 
-showPiece :: PColor -> PType -> String 
-showPiece White Pawn = "♙"
-showPiece White Bishop = "♗"
-showPiece White Rook = "♖"
-showPiece White Knight = "♘"
-showPiece White King = "♔"
-showPiece White Queen = "♕"
-showPiece Black Pawn = "♟"
-showPiece Black Bishop = "♝"
-showPiece Black Rook = "♜"
-showPiece Black Knight = "♞"
-showPiece Black King = "♚"
-showPiece Black Queen = "♛"
+
+
+showPiece :: Piece -> String 
+showPiece (Piece White Pawn) = "♙"
+showPiece (Piece White Bishop) = "♗"
+showPiece (Piece White Rook) = "♖"
+showPiece (Piece White Knight) = "♘"
+showPiece (Piece White King) = "♔"
+showPiece (Piece White Queen) = "♕"
+showPiece (Piece Black Pawn) = "♟"
+showPiece (Piece Black Bishop) = "♝"
+showPiece (Piece Black Rook) = "♜"
+showPiece (Piece Black Knight) = "♞"
+showPiece (Piece Black King) = "♚"
+showPiece (Piece Black Queen) = "♛"
 
  
 
