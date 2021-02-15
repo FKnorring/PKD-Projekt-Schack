111969fde1398b3aa3c8204536760216a3852264
eriklarsson00 <71325754+eriklarsson00@users.noreply.github.com>
2021-02-15 17:05:06 +0100

    Add files via upload
    li

--- projekt.hs
+++ projekt.hs
@@ -1,4 +1,30 @@
-type Board = (Char, int, [Square])
-Data PType = Bishop | Pawn | Rook | Knight | King | Queen
-Data PColor = White | Black
-type Piece = PColor PType
+
+{-data PType represents the different piece types-}
+data PType = Bishop | Pawn | Rook | Knight | King | Queen
+
+{- data PColor represent the pieces color-}
+data PColor = White | Black
+
+{-data Square represents if a certain square is empty or contains a piece-}
+data Square =  Empty | Piece
+
+
+{- data Piece represents the specific piece-}
+
+
+showPiece :: PColor -> PType -> String 
+showPiece White Pawn = "wP"
+showPiece White Bishop = "wB"
+showPiece White Rook = "wR"
+showPiece White Knight = "wKN"
+showPiece White King = "wK"
+showPiece White Queen = "wQ"
+showPiece Black Pawn = "bP"
+showPiece Black Bishop = "bB"
+showPiece Black Rook = "bR"
+showPiece Black Knight = "bKn"
+showPiece Black King = "bK"
+showPiece Black Queen = "bQ"
+
+ 
+
