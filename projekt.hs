91621ac8c6b55038b3b446455f98960f248dfc6e
FKnorring <71324888+FKnorring@users.noreply.github.com>
2021-02-15 17:47:20 +0100

    Update projekt.hs

--- projekt.hs
+++ projekt.hs
@@ -1,4 +1,6 @@
 import Data.Char
+import Control.Monad
+import Data.List
 
 type Board = [[Square]]
 
@@ -14,25 +16,39 @@ data PType = Bishop | Pawn | Rook | Knight | King | Queen
 {- data PColor represent the pieces color-}
 data PColor = White | Black
 
+instance Show Square where
+    show Empty = " "
 
+instance Show Piece where
+    show (Piece White Pawn) = "♙"
+    show (Piece White Bishop) = "♗"
+    show (Piece White Rook) = "♖"
+    show (Piece White Knight) = "♘"
+    show (Piece White King) = "♔"
+    show (Piece White Queen) = "♕"
+    show (Piece Black Pawn) = "♟"
+    show (Piece Black Bishop) = "♝"
+    show (Piece Black Rook) = "♜"
+    show (Piece Black Knight) = "♞"
+    show (Piece Black King) = "♚"
+    show (Piece Black Queen) = "♛"
 
+initBoard = [[Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty],
+             [Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty],
+             [Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty],
+             [Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty],
+             [Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty],
+             [Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty],
+             [Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty],
+             [Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty]]
 
-
-
-
-showPiece :: Piece -> String 
-showPiece (Piece White Pawn) = "♙"
-showPiece (Piece White Bishop) = "♗"
-showPiece (Piece White Rook) = "♖"
-showPiece (Piece White Knight) = "♘"
-showPiece (Piece White King) = "♔"
-showPiece (Piece White Queen) = "♕"
-showPiece (Piece Black Pawn) = "♟"
-showPiece (Piece Black Bishop) = "♝"
-showPiece (Piece Black Rook) = "♜"
-showPiece (Piece Black Knight) = "♞"
-showPiece (Piece Black King) = "♚"
-showPiece (Piece Black Queen) = "♛"
-
+drawGrid :: Board -> IO ()
+drawGrid board = do
+    forM_ [0..7] $ \x ->
+      forM_ [0..7] $ \y -> do
+         putStrLn "Here's a square!"
+         putStrLn ("Square(" ++ show x ++ ", " ++ show y ++ ")")
+         -- add here the actual drawing Gloss commands
+    
  
 
