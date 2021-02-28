4390433a34763869951cbd9abd0259b5bf0c6896
FKnorring <71324888+FKnorring@users.noreply.github.com>
2021-02-28 16:59:02 +0100

    latjj

--- gloss.hs
+++ gloss.hs
@@ -4,7 +4,16 @@ import Chess hiding (main, play)
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 
+data State = Running | GameOver (Maybe PColor) deriving (Eq,Show)
+
+data Game = Game { gameBoard :: Board,
+                   gamePlayer :: PColor,
+                   gameState :: State
+                    } deriving (Eq, Show)
+
 window =  InWindow "Chesskell" (480, 480) (100, 100)
+
+initGame = Game { gameBoard = initBoard, gamePlayer = White, gameState = Running}
 
 main :: IO ()
 main = do
@@ -21,11 +30,17 @@ main = do
     blackking <- loadBMP "blackking.bmp"
     blackqueen <- loadBMP "blackqueen.bmp"
     empty <- loadBMP "empty.bmp"
-    let brd = testBoard
-    display window white (renderBoard brd [whitepawn, whitebishop,whiterook,whiteknight,whiteking,whitequeen,blackpawn,blackbishop,blackrook,blackknight,blackking,blackqueen,empty])
+    let pieces = [whitepawn, whitebishop,whiterook,whiteknight,whiteking,whitequeen,blackpawn,blackbishop,blackrook,blackknight,blackking,blackqueen,empty]
+    play window white 30 initGame (renderBoard pieces) getClick (const id)
+
+renderGame :: Game -> Picture 
+renderGame game =
+    case gameState game of 
+        Running -> renderBoard (gameBoard game)
+        GameOver winner -> gameOverBoard winner (gameBoard game)
 
 renderBoard :: Board -> [Picture] -> Picture
-renderBoard brd imgs =
+renderBoard imgs brd =
  pictures $ [translate (fromIntegral $ -210+60*x) (fromIntegral $ 210-60*y) $
         case sqr of
             Piece White (Pawn _) -> imgs !! 0
@@ -44,9 +59,29 @@ renderBoard brd imgs =
         | x <- [0..7], y <- [0..7], sqr <- [(brd !! y) !! x]]
 
 
-parseCoordinate crd = undefined
+getfstClick :: Event -> Game -> Game
+getfstClick (EventKey (MouseButton LeftButton) Down _ (x, y)) game = 
+    case gameState game of 
+        Running -> playerTurn' game $ parseCoord (x,y)
+        _ -> game
+getfstClick _ game = game
 
-handleKeys :: Event -> Board -> Board
-handleKeys (EventKey (MouseButton LeftButton) Down _ (x, y)) b = undefined
+getSndClick :: Event -> Coordinate -> Game -> Game
+getSndClick (EventKey (MouseButton LeftButton) Down _ (x, y)) crd1 game = playerTurn'' game crd1 $ parseCoord (x,y)
+
+parseCoord :: (Float,Float) -> Coordinate
+parseCoord (x,y) = (floor (x + 210)/60,floor (y + 210)/60)
+
+playerTurn' :: Game -> Coordinate -> Game
+playerTurn' game cord = case getSquare cord brd of
+        Empty -> getfstClick game
+        (Piece (other clr) _ ) -> getfstClick game
+        _ -> getSndClick cord game
+    where brd = gameBoard game
+          clr = gamePlayer game
+
+playerTurn'' :: Game -> Coordinate -> Coordinate -> Game
+playerTurn'' game crd1 crd2 = gameBoard 
+
 
 move = undefined
