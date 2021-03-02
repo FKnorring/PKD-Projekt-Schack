d955729f3b20d21a5b532cf121fa1904f66cc5b4
eriklarsson00 <71325754+eriklarsson00@users.noreply.github.com>
2021-03-02 16:00:59 +0100

    Update Chess.hs

--- Chess.hs
+++ Chess.hs
@@ -414,7 +414,7 @@ testBoard2 = [[Piece White (Pawn SingleMove),Piece Black Knight,Piece Black Bish
              [Piece White (Rook Unmoved ),Piece White Knight,Piece White Bishop,Piece White Queen,Piece White (King Unmoved),Piece White Bishop,Piece White Knight,Piece White (Rook Unmoved )]]
 
 
-{- performTests = runTestTT $ TestList [test1,test2,test3,test4,test5,test6,test7,test8,test9,test10,test11,test12,test13,test14,test15,test16]
+{- performTests = runTestTT $ TestList [test1,test2,test3,test4,test5,test6,test7,test8,test9,test10,test11,test12,test13,test14,test15,test16,test17]
 
 test1 = TestCase $ assertEqual "Looks if the safe Black king is in check on startboard" False (isChecked White initBoard)
 test2 = TestCase $ assertEqual "Looks if the white king is in check on testboard" True (isChecked White testBoard)
